43309bff9bb8077bcdfa5d78d93f27fd560e6191
Yash Sangole <yashsangole09@gmail.com>
2025-04-23 12:53:09 +0530

    Add files via upload

---
+++ program204.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int SumFactors(int iNo)
+{
+    int iCnt = 0, iSum = 0;
+
+    for(iCnt = 1; iCnt <= iNo / 2; iCnt++)
+    {
+        if((iNo % iCnt) == 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    cout<<"Enter number : "<<endl;
+    cin>>iValue;
+
+    iRet = SumFactors(iValue);
+
+    cout<<"Summation of factors is : "<<iRet<<endl;
+
+    return 0;
+}
